69ff2bbc518a2e79532e215d2eba7413583855b7
Amina Saidaxmedova <aminasaidakhmedova@gmail.com>
2024-07-02 15:36:27 +0500

    added entity Citizen, made getters and setters for its members, made ID int, positive and unchangeable + program assigns ID, not user

---
+++ src/main/java/uzb/aminasaidakhmedova/government/entity/Citizen.java
@@ -0,0 +1,52 @@
+package uzb.aminasaidakhmedova.government.entity;
+
+public class Citizen {
+
+    private static int newId = 1;
+    private final int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Country country;
+
+    public Citizen (String firstName, String lastName, int age, Country country) {
+        this.id = newId++;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.country = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void getAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+}
