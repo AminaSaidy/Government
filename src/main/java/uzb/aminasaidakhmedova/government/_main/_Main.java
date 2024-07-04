97af7406983f91645c5010ce48f50e3c9ba74ba0
Amina Saidaxmedova <aminasaidakhmedova@gmail.com>
2024-07-04 09:55:19 +0500

    created a menu with an infinite loop

--- src/main/java/uzb/aminasaidakhmedova/government/_main/_Main.java
+++ src/main/java/uzb/aminasaidakhmedova/government/_main/_Main.java
@@ -2,10 +2,13 @@ package uzb.aminasaidakhmedova.government._main;
 
 import uzb.aminasaidakhmedova.government.entity.*;
 
+import java.util.Scanner;
+
 public class _Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Country country = Country.getCountry("Greatland", "Greatcapital");
-        Population population = new Population ();
+        Population population = new Population();
         State state1 = new State("New Rolly");
         State state2 = new State("Yorkes");
         City city1 = new City("London");
@@ -20,12 +23,40 @@ public class _Main {
         country.addState(state1);
         country.addState(state2);
 
-        for (int i = 0; i<5; i++) {
-            Citizen citizen = new Citizen (country);
+        for (int i = 0; i < 5; i++) {
+            Citizen citizen = new Citizen(country);
             population.addCitizens(citizen);
         }
 
-        System.out.println(country);
-        System.out.println(population);
+        while (true) {
+            System.out.println("Choose the option: ");
+            System.out.println("1.Print the capital.");
+            System.out.println("2.Print amount of states.");
+            System.out.println("3.Print the area of the country.");
+            System.out.println("4.Print states' centres.");
+            System.out.println("5.Print average age of citizens.");
+            System.out.println("6.Print citizens with certain length of their first names.");
+            System.out.println("7.Print citizens whose first names start with a certain character.");
+
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                default:
+                    System.out.println("Wrong input. Try again.");
+            }
+        }
     }
 }
